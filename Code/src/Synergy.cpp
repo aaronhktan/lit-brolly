b5f3deb594b08d7e218ee975b8a970b839250947
Aaron Tan <cheeseisdisgusting@gmail.com>
2017-12-04 02:40:17 -0500

    Updated to use abstract classes
    You get a virtual function! You get a virtual function! Everybody gets a virtual function!

--- Code/src/Synergy.cpp
+++ Code/src/Synergy.cpp
@@ -4,129 +4,147 @@
 #define NEOPIXEL_I2C_DEVICE_NUM 0
 #define DEV_ADDRESS 0x08
 
+// Abstract Accelerometer class
 class Accelerometer {
-	public:
-		int get_x_accel();
-		int get_y_accel();
-		int get_z_accel();
-		Accelerometer();
-		~Accelerometer();
-	private:
-		int status, x_accel, y_accel, z_accel;
-		uint8_t *buffer0, *buffer1; // buffer0 contains least significant bit, buffer1 contains most significant bit
+public:
+    // Pure virtual functions to get x, y, and z accelerations
+    virtual int get_x_accel() = 0;
+    virtual int get_y_accel() = 0;
+    virtual int get_z_accel() = 0;
 };
 
-// Setup the accelerometer
-Accelerometer::Accelerometer() {
-	// Initialize some variables
-	status = 0;
-	buffer0 = new uint8_t[2];
-	buffer1 = new uint8_t[2];
+// Derived class ADXL345 is a subclass of the Accelerometer 
+class ADXL345 : public Accelerometer {
+public:
+    // Concrete implementation of virtual functions
+    int get_x_accel();
+    int get_y_accel();
+    int get_z_accel();
 
-	// i2c_write(devNum, devAddr, addr, val)
-	// The ADXL345 has an address of 0x53 (83); writing 0x0A (10) to 0x2C (44, bandwidth rate register) sets the output data rate to 100Hz.
-	status = i2c_write(0, 0x53, 0x2C, 0x0A);
+    // Constructor and destructors
+    ADXL345();
+    ~ADXL345();
+private:
+    int status, x_accel, y_accel, z_accel;
+    uint8_t *buffer0, *buffer1; // buffer0 contains least significant bit, buffer1 contains most significant bit
+};
 
-	// Writing 0x08 (08) to 0x2D (45, power control register) disables auto-sleep.
-	status = i2c_write(0, 0x53, 0x2D, 0x08);
+// Setup the ADXL345
+ADXL345::ADXL345() {
+    // Initialize some variables
+    status = 0;
+    buffer0 = new uint8_t[2];
+    buffer1 = new uint8_t[2];
 
-	// Writing 0x08 (08) to 0x31 (49) sets the sensitivity to +/-2g, with self-test disabled and 4-wire interface.
-	status = i2c_write(0, 0x53, 0x31, 0x08);
+    // i2c_write(devNum, devAddr, addr, val)
+    // The ADXL345 has an address of 0x53 (83); writing 0x0A (10) to 0x2C (44, bandwidth rate register) sets the output data rate to 100Hz.
+    status = i2c_write(0, 0x53, 0x2C, 0x0A);
 
-	std::cout << "Status after setup is " << status << std::endl;
+    // Writing 0x08 (08) to 0x2D (45, power control register) disables auto-sleep.
+    status = i2c_write(0, 0x53, 0x2D, 0x08);
+
+    // Writing 0x08 (08) to 0x31 (49) sets the sensitivity to +/-2g, with self-test disabled and 4-wire interface.
+    status = i2c_write(0, 0x53, 0x31, 0x08);
+
+    std::cout << "Status after setup is " << status << std::endl;
 }
 
 // Clean up after ourselves!
-Accelerometer::~Accelerometer() {
-	delete[] buffer0;
-	delete[] buffer1;
+ADXL345::~ADXL345() {
+    delete[] buffer0;
+    delete[] buffer1;
 }
 
 // Get x-values
-int Accelerometer::get_x_accel() {
-	status = i2c_read(0, 0x53, 0x32, buffer0, 1); // i2c_read(devNum, devAddr, addr, buffer, numbytes)
-	status = i2c_read(0, 0x53, 0x33, buffer1, 1); // 0x32 to 0x37 contain acceleration data
+int ADXL345::get_x_accel() {
+    status = i2c_read(0, 0x53, 0x32, buffer0, 1); // i2c_read(devNum, devAddr, addr, buffer, numbytes)
+    status = i2c_read(0, 0x53, 0x33, buffer1, 1); // 0x32 to 0x37 contain acceleration data
 
-	// Now that we have the data, convert it to 10-bits
-	int x_accel = (buffer1[0] & 0x03 * 256) + buffer0[0];
-	if (x_accel > 511) {
-		x_accel -= 1024;
-	}
+    // Now that we have the data, convert it to 10-bits with some magic numbers
+    int x_accel = (buffer1[0] & 0x03 * 256) + buffer0[0];
+    if (x_accel > 511) {
+        x_accel -= 1024;
+    }
 
-	return x_accel;
+    return x_accel;
 }
 
 // Get y-values
-int Accelerometer::get_y_accel() {
-	status = i2c_read(0, 0x53, 0x34, buffer0, 1);
-	status = i2c_read(0, 0x53, 0x35, buffer1, 1);
+int ADXL345::get_y_accel() {
+    status = i2c_read(0, 0x53, 0x34, buffer0, 1);
+    status = i2c_read(0, 0x53, 0x35, buffer1, 1);
 
-	int y_accel = (buffer1[0] & 0x03 * 256) + buffer0[0];
-	if (y_accel > 511) {
-		y_accel -= 1024;
-	}
+    int y_accel = (buffer1[0] & 0x03 * 256) + buffer0[0];
+    if (y_accel > 511) {
+        y_accel -= 1024;
+    }
 
-	return y_accel;
+    return y_accel;
 }
 
 // Get z-values
-int Accelerometer::get_z_accel() {
-	status = i2c_read(0, 0x53, 0x36, buffer0, 1);
-	status = i2c_read(0, 0x53, 0x37, buffer1, 1);
+int ADXL345::get_z_accel() {
+    status = i2c_read(0, 0x53, 0x36, buffer0, 1);
+    status = i2c_read(0, 0x53, 0x37, buffer1, 1);
 
-	int z_accel = (buffer1[0] & 0x03 * 256) + buffer0[0];
-	if (z_accel > 511) {
-		z_accel -= 1024;
-	}
+    int z_accel = (buffer1[0] & 0x03 * 256) + buffer0[0];
+    if (z_accel > 511) {
+        z_accel -= 1024;
+    }
 
-	return z_accel;
+    return z_accel;
 }
 
+/*  Struct declaration to hold data about how to light the Neopixels
+    Typedef for convenience when initializing */
 typedef struct Pixel {
-	int brightness;
-	int r;
-	int g;
-	int b;
+    int brightness;
+    int r;
+    int g;
+    int b;
 } Pixel;
 
 int main(const int argc, const char* const argv[]) {
+    ADXL345 accelerometer; // Instantiate ADXL345 object (and set up the accelerometer)
 
-	Accelerometer accelerometer; // Instantiate Accelerometer object (and set up the accelerometer)
+    sleep(1); // Sleep for a second to make sure that changes have propagated
 
-	usleep(500); // Sleep for 0.5 seconds to make sure that changes have propagated
+    // Infinite loop to continuously grab data from the accelerometer and then push the changes to the LEDs
+    for (int i = 0; i > -1; i += 0) {
+        std::cout << "Looping..." << std::endl;
 
-	// Allocate memory for buffer and read from accelerometer into the buffer
-	// 0x00 contains all the data from the accelerometer.
-	for (int i = 0; i > -1; i += 0) {
+        // Initialize a pixel with default brightness values and RGB values based on the X, Y, and Z readings from accelerometer
+        Pixel pixel;
+        pixel.brightness = 127;
+        pixel.r = accelerometer.get_x_accel();
+        pixel.g = accelerometer.get_y_accel();
+        pixel.b = accelerometer.get_z_accel();
 
-		std::cout << "Looping..." << std::endl;
+        // FOR TESTING PURPOSES DO NOT UNCOMMENT UNLESS YOU KNOW WHAT YOU'RE DOING
+        // i2c_writeBytes(NEOPIXEL_I2C_DEVICE_NUM, DEV_ADDRESS, pixel.brightness, 0x01, 0);
+        // i2c_writeBytes(NEOPIXEL_I2C_DEVICE_NUM, DEV_ADDRESS, 255, 0x01, 0);
+        // i2c_writeBytes(NEOPIXEL_I2C_DEVICE_NUM, DEV_ADDRESS, 0, 0x01, 0);
+        // i2c_writeBytes(NEOPIXEL_I2C_DEVICE_NUM, DEV_ADDRESS, 0, 0x01, 0);
 
-		// Get arguments from accelerometer
-		Pixel pixel;
-		pixel.brightness = 127;
-		pixel.r = accelerometer.get_x_accel();
-		pixel.g = accelerometer.get_y_accel();
-		pixel.b = accelerometer.get_z_accel();
+        /*  Use I2C to communicate with the Arduino dock: change values with #defines at top of file
+            int i2c_writeBytes (int devNum, int devAddr, int addr, int val, int numBytes)
+            devNum is always 0 for Arduino dock
+            devAddr for the ATMega is 0x08 by default unless you change it on the Omega itself
+            addr to write to is the value that we want to transmit to the microcontroller, to be read with Wire library
+            val to be written is a dummy value because we specify that we write 0 bytes of data */
+        i2c_writeBytes(NEOPIXEL_I2C_DEVICE_NUM, DEV_ADDRESS, pixel.brightness, 0x01, 0);
+        i2c_writeBytes(NEOPIXEL_I2C_DEVICE_NUM, DEV_ADDRESS, pixel.r, 0x01, 0);
+        i2c_writeBytes(NEOPIXEL_I2C_DEVICE_NUM, DEV_ADDRESS, pixel.g, 0x01, 0);
+        i2c_writeBytes(NEOPIXEL_I2C_DEVICE_NUM, DEV_ADDRESS, pixel.b, 0x01, 0);
 
-		// i2c_writeBytes(NEOPIXEL_I2C_DEVICE_NUM, DEV_ADDRESS, pixel.brightness, 0x01, 0);
-		// i2c_writeBytes(NEOPIXEL_I2C_DEVICE_NUM, DEV_ADDRESS, 255, 0x01, 0);
-		// i2c_writeBytes(NEOPIXEL_I2C_DEVICE_NUM, DEV_ADDRESS, 0, 0x01, 0);
-		// i2c_writeBytes(NEOPIXEL_I2C_DEVICE_NUM, DEV_ADDRESS, 0, 0x01, 0);
+        // Print out the data for verification
+        std::cout << "Acceleration in the X-Axis: " << pixel.r << std::endl;
+        std::cout << "Acceleration in the Y-Axis: " << pixel.g << std::endl;
+        std::cout << "Acceleration in the Z-Axis: " << pixel.b << std::endl;
 
-		i2c_writeBytes(NEOPIXEL_I2C_DEVICE_NUM, DEV_ADDRESS, pixel.brightness, 0x01, 0);
-		i2c_writeBytes(NEOPIXEL_I2C_DEVICE_NUM, DEV_ADDRESS, pixel.r, 0x01, 0);
-		i2c_writeBytes(NEOPIXEL_I2C_DEVICE_NUM, DEV_ADDRESS, pixel.g, 0x01, 0);
-		i2c_writeBytes(NEOPIXEL_I2C_DEVICE_NUM, DEV_ADDRESS, pixel.b, 0x01, 0);
+        // Sleep for a second to make it look nicer
+        sleep(1);
+    }
 
-		// Print out the data
-		std::cout << "Acceleration in the X-Axis: " << pixel.r << std::endl;
-		std::cout << "Acceleration in the Y-Axis: " << pixel.g << std::endl;
-		std::cout << "Acceleration in the Z-Axis: " << pixel.b << std::endl;
-
-		// Sleep for a second to make it look nicer
-		sleep(1);
-
-	}
-
-	return 0;
+    return 0;
 }
--- Code/src/sketch_dec03a.ino
+++ Code/src/sketch_dec03a.ino
@@ -4,17 +4,19 @@
   #include <avr/power.h>
 #endif
 
-#define PIN0 6
-#define PIN1 7
-#define PIN2 8
-#define PIN3 9
-#define PIN4 10
-#define PIN5 11
-#define PIN6 12
-#define PIN7 13
+// Data pins for the eight Neopixel strands
+#define PIN_NEO_0 6
+#define PIN_NEO_1 7
+#define PIN_NEO_2 8
+#define PIN_NEO_3 9
+#define PIN_NEO_4 10
+#define PIN_NEO_5 11
+#define PIN_NEO_6 12
+#define PIN_NEO_7 13
 
 #define NUMBER_OF_STRANDS 8
 
+// Struct declaration, where member variables change how a pixel is lit
 typedef struct Pixel {
   int r;
   int g;
@@ -22,41 +24,46 @@ typedef struct Pixel {
   int brightness;
 } Pixel;
 
+// Initialize an array of strands with each strand's data pin set
 Adafruit_NeoPixel strip[NUMBER_OF_STRANDS] = {
-  Adafruit_NeoPixel(60, PIN0, NEO_GRB + NEO_KHZ800),
-  Adafruit_NeoPixel(60, PIN1, NEO_GRB + NEO_KHZ800),
-  Adafruit_NeoPixel(60, PIN2, NEO_GRB + NEO_KHZ800),
-  Adafruit_NeoPixel(60, PIN3, NEO_GRB + NEO_KHZ800),
-  Adafruit_NeoPixel(60, PIN4, NEO_GRB + NEO_KHZ800),
-  Adafruit_NeoPixel(60, PIN5, NEO_GRB + NEO_KHZ800),
-  Adafruit_NeoPixel(60, PIN6, NEO_GRB + NEO_KHZ800),
-  Adafruit_NeoPixel(60, PIN7, NEO_GRB + NEO_KHZ800)
+  Adafruit_NeoPixel(60, PIN_NEO_0, NEO_GRB + NEO_KHZ800),
+  Adafruit_NeoPixel(60, PIN_NEO_1, NEO_GRB + NEO_KHZ800),
+  Adafruit_NeoPixel(60, PIN_NEO_2, NEO_GRB + NEO_KHZ800),
+  Adafruit_NeoPixel(60, PIN_NEO_3, NEO_GRB + NEO_KHZ800),
+  Adafruit_NeoPixel(60, PIN_NEO_4, NEO_GRB + NEO_KHZ800),
+  Adafruit_NeoPixel(60, PIN_NEO_5, NEO_GRB + NEO_KHZ800),
+  Adafruit_NeoPixel(60, PIN_NEO_6, NEO_GRB + NEO_KHZ800),
+  Adafruit_NeoPixel(60, PIN_NEO_7, NEO_GRB + NEO_KHZ800)
 };
 
 int transmission_number = 0;
-Pixel pixel;
+Pixel pixel, previous_pixel;
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT); // Turn off that blue LED on the Arduino dock (because it is ugly)
   digitalWrite(LED_BUILTIN, LOW);
 
-  Wire.begin(0x08);
+  Wire.begin(0x08); // Listen for input on 0x08, the device address of the Arduino dock
   Wire.onReceive(i2c_on_receive_handler);
 
+  // Loop through all the strands to set them up
   for (int i = 0; i < NUMBER_OF_STRANDS; i++) {
-    strip[i].begin();
-    strip[i].show();
+    strip[i].begin(); // Prepares data pin for Neopixel output
+    strip[i].show(); // Turns off all Neopixels at the beginning
   }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // Nothing to see here friendos! :)
 }
 
+// Fires when data is passed from the Omega to the Arduino dock
 void i2c_on_receive_handler(int bytes_num) {
-//  digitalWrite(LED_BUILTIN, Wire.read());
-
+  
+  /* Depending on which transmission over I2C this is, we set different member variables of the Pixel
+   * First, brightness, then R, G, B, and finally, we show the pixels.
+   */
+   
   switch(transmission_number) {
     case 0:
       pixel.brightness = Wire.read();
@@ -72,15 +79,53 @@ void i2c_on_receive_handler(int bytes_num) {
       break;
     case 3:
       pixel.b = Wire.read();
-      transmission_number++;
-      for (uint16_t i = 0; i < strip[0].numPixels(); i++) {
-        for (int j = 0; j < NUMBER_OF_STRANDS; j++) {
-          strip[j].setPixelColor(i, pixel.r, pixel.g, pixel.b);
-          strip[j].setBrightness(pixel.brightness);
-          strip[j].show();
+//      color_transition(pixel.r, pixel.g, pixel.b, pixel.brightness);
+      for (int i = 0; i < NUMBER_OF_STRANDS; i++) { // Loop through every strand and every pixel to set colour
+        for (uint16_t j = 0; j < strip[i].numPixels(); j++) {
+          strip[i].setPixelColor(j, pixel.r, pixel.g, pixel.b);
+          strip[i].show();
         }
       }
       transmission_number = 0;
       break;
   }
 }
+
+//void color_transition(int r, int g, int b, int brightness) {
+//  for (int i = 0; i <= 255; i++) {
+//    if (previous_pixel.r < r && i > previous_pixel.r && pixel.r < r) {
+//      pixel.r++;
+//    } else if (previous_pixel.r > r && i > r && pixel.r > r) {
+//      pixel.r--;
+//    }
+//
+//    if (previous_pixel.g < g && i > previous_pixel.g && pixel.g < g) {
+//      pixel.g++;
+//    } else if (previous_pixel.g > g && i > g && pixel.g > g) {
+//      pixel.g--;
+//    }
+//
+//    if (previous_pixel.b < b && i > previous_pixel.b && pixel.b < b) {
+//      pixel.b++;
+//    } else if (previous_pixel.b > b && i > b && pixel.b > b) {
+//      pixel.b--;
+//    }
+//
+//    for (int i = 0; i < NUMBER_OF_STRANDS; i++) { // Loop through every strand and every pixel to set colour
+//      for (uint16_t j = 0; j < strip[i].numPixels(); j++) {
+//        strip[i].setPixelColor(j, pixel.r, pixel.g, pixel.b);
+//        strip[i].show();
+//      }
+//    }
+//    
+//    delay(3);
+//  }
+//  
+//  previous_pixel = {
+//    .r = r,
+//    .g = g,
+//    .b = b,
+//    .brightness = brightness
+//  };
+//}
+
